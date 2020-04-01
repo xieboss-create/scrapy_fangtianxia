e709b9a63ffca70743478c0634299202a3e9bc37
82FB9888E5BAA19471E3D52771AF783F <82FB9888E5BAA19471E3D52771AF783F@i-search.com.cn>
2020-04-01 16:37:02 +0800

    '导出依赖'

--- fang/fang/pipelines.py
+++ fang/fang/pipelines.py
@@ -19,8 +19,9 @@ class FangPipeline(object):
     def close_spider(self, spider):
         self.fp.close()
 
+
 class SaveDataPipeline(object):
-    def open_spider(self,spider):
+    def open_spider(self, spider):
         # 会把settings中的所有的等号左边的值当作key  等号右边的值当作value
         settings = get_project_settings()
         db_host = settings['DB_HOST']
@@ -31,22 +32,23 @@ class SaveDataPipeline(object):
         db_charset = settings['DB_CHARSET']
 
         self.conn = pymysql.Connect(host=db_host,
-                                   user=db_user,
-                                   password=db_password,
-                                   database=db_name,
-                                   # 端口号必须是整型
-                                   port=db_port,
-                                   # 字符集不允许加-
-                                   charset=db_charset)
+                                    user=db_user,
+                                    password=db_password,
+                                    database=db_name,
+                                    # 端口号必须是整型
+                                    port=db_port,
+                                    # 字符集不允许加-
+                                    charset=db_charset)
         self.cursor = self.conn.cursor()
 
-    def process_item(self,item,spider):
+    def process_item(self, item, spider):
         # {}的外面必须要加 “”
-        sql = 'insert into `fangtianxia` values ("{}","{}","{}","{}")'.format(item['province'],item['city'],item['name'],item['price'])
+        sql = 'insert into `fangtianxia` values ("{}","{}","{}","{}")'.format(item['province'], item['city'],
+                                                                              item['name'], item['price'])
         self.cursor.execute(sql)
         self.conn.commit()
         return item
 
-    def close_spider(self,spider):
+    def close_spider(self, spider):
         self.cursor.close()
         self.conn.close()
